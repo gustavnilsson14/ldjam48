f866fcae33a3ebaca410351aaf5c8a2aef2a8688
KreXor <KreXor@gmail.com>
2021-04-24 22:28:15 +0200

    animations

---
+++ Assets/Scripts/EntityWorldRenderer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityWorldRenderer : MonoBehaviour
+{
+    public GameObject EntityParticlePrefab;
+
+    private void Start()
+    {
+        Render();
+    }
+    public void Render()
+    {
+        Instantiate(EntityParticlePrefab);
+    }
+
+    public void Attack()
+    {
+
+    }
+}
