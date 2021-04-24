dbdd8d8c756612eb3dd903955815e987e360c44d
Gustav Nilsson <gustavnilsson14@gmail.com>
2021-04-24 12:30:45 +0200

    terminal get

--- Assets/Scripts/IOTerminal.cs
+++ Assets/Scripts/IOTerminal.cs
@@ -1,18 +1,80 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+using UnityEngine.EventSystems;
 
 public class IOTerminal : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static IOTerminal I;
+    public TextMeshProUGUI outputField;
+    public TextMeshProUGUI userDirField;
+    public TMP_InputField commandField;
+
+    private string baseUserDirString;
+
+    private void Awake()
     {
-        
+        IOTerminal.I = this;
+        commandField.onSubmit.AddListener(onCommandSubmit);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        RenderUserAndDir();
+        ResetCommandField();
+    }
+
+    private void RenderUserAndDir()
+    {
+        baseUserDirString = userDirField.text;
+        userDirField.text = String.Format(baseUserDirString, "haxxor", "LDTerminal", "CSOS128", "");
+    }
+
+    private void onCommandSubmit(string commandName)
+    {
+        HandleStringInput(commandName);
+        ResetCommandField();
+    }
+
+    private void ResetCommandField()
+    {
+        commandField.text = "";
+        commandField.ActivateInputField();
+    }
+
+    private void HandleStringInput(string commandName)
+    {
+        if (!Player.GetCommand(out Command command, commandName))
+        {
+            HandleNoSuchCommand(commandName);
+            return;
+        }
+        HandleCommand(command);
+    }
+
+    private void HandleCommand(Command command)
+    {
+        AppendDefaultCommandText();
+        AppendTextLine("HELLZ YAH " + command.name);
+    }
+
+    private void HandleNoSuchCommand(string commandName)
+    {
+        AppendDefaultCommandText();
+        AppendTextLine("No such command");
+    }
+
+    private void AppendDefaultCommandText()
+    {
+        AppendTextLine(userDirField.text);
+        AppendTextLine("$ " + commandField.text);
+    }
+
+    private void AppendTextLine(string newText)
+    {
+        outputField.text += "\n" + newText;
     }
 }
