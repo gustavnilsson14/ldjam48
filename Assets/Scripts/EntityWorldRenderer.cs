b8a3738c754d4b5bc48f9cea2854835137fffa70
KreXor <KreXor@gmail.com>
2021-04-24 23:04:14 +0200

    entity animations

--- Assets/Scripts/EntityWorldRenderer.cs
+++ Assets/Scripts/EntityWorldRenderer.cs
@@ -1,22 +1,56 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EntityWorldRenderer : MonoBehaviour
 {
-    public GameObject EntityParticlePrefab;
+    public GameObject entityParticlePrefab;
+
+    private Entity entity;
+    private GameObject entityParticle;
+    private Animator animator;
 
     private void Start()
     {
         Render();
+
+        entity = GetComponent<Entity>();
+        if (entity == null)
+            return;
+
+        entity.onTakeDamage.AddListener(TakeDamage);
+        entity.onDeath.AddListener(Die);
     }
+
+   
     public void Render()
     {
-        Instantiate(EntityParticlePrefab);
+        entityParticle = Instantiate(entityParticlePrefab);
+        animator = entityParticle.GetComponent<Animator>();
     }
 
     public void Attack()
     {
+        RunAnimation("Attack");
+    }
 
+    public void TakeDamage(int damage)
+    {
+        RunAnimation("TakeDamage");
+    }
+
+    private void Die()
+    {
+        RunAnimation("Die");
+    }
+
+
+    public void RunAnimation(string animation)
+    {
+        if (animator == null)
+            return;
+
+        animator.SetTrigger(animation);
     }
 }
