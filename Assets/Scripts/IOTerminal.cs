5e593a216f3204306e91dd81195fa14428dee1c4
KreXor <KreXor@gmail.com>
2021-04-26 16:40:12 +0200

    can autocomplete player

--- Assets/Scripts/IOTerminal.cs
+++ Assets/Scripts/IOTerminal.cs
@@ -142,7 +142,7 @@ public class IOTerminal : MonoBehaviour
     {
         List<Entity> entities = new List<Entity>();
         entities.AddRange(Player.I.currentDirectory.GetEntities());
-        entities.Remove(Player.I);
+        //entities.Remove(Player.I);
         if (entities.Count == 0)
             return;
 
